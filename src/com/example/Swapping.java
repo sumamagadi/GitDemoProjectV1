99fc64cb2475b90c0cc4cfa47fda3acd27a74233
Suma M <sumamagadi95@gmail.com>
2023-09-11 12:38:31 +0530

    updated...

--- src/com/example/Swapping.java
+++ src/com/example/Swapping.java
@@ -2,19 +2,20 @@ package com.example;
 
 class Swapping {
 
-	  public static void main(String[] args) {
-	    int a = 11, b = 22;
+	public static void main(String[] args) {
+		int a = 11, b = 22;
 
-	    System.out.println("Before swapping the numbers:");
-	    System.out.println("First number = " + a);
-	    System.out.println("Second number = " + b);
+		System.out.println("Before swapping the numbers:");
+		System.out.println("First number = " + a);
+		System.out.println("Second number = " + b);
 
-	    int temp = a; 
-	    a = b; 
-	    b = temp; 
+		int temp = a;
+		a = b;
+		b = temp;
 
-	    System.out.println("After swapping the numbers: ");
-	    System.out.println("First number = " + a);
-	    System.out.println("Second number = " + b);
-	  }
+		System.out.println("After swapping the numbers: ");
+		System.out.println("First number = " + a);
+		System.out.println("Second number = " + b);
 	}
+}
+//New coomit
